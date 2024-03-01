bb6881f35a2a97468014640e2b5af4e3af415697
Zachary <1903253513@qq.com>
2024-03-01 16:00:50 +0800

    添加tcp上下载、上传文件模块代码

--- idc/cpp/stop.sh
+++ idc/cpp/stop.sh
@@ -5,7 +5,9 @@ killall -9 procctl
 
 #停止其他服务程序
 killall crtsurfdata deletefiles gzipfiles ftpgetfile ftpputfiles
+killall fileserver tcpputfiles tcpgetfiles
 
 sleep 5
 
 killall -9 crtsurfdata deletefiles gzipfiles ftpgetfile ftpputfiles
+killall -9 fileserver tcpputfiles tcpgetfiles
