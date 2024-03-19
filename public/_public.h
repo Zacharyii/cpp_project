0c00f81ab3f32919c7fcc6e09f6a5b31ae966a07
Zachary <1903253513@qq.com>
2024-03-19 23:53:17 +0800

    信号量操作

--- public/_public.h
+++ public/_public.h
@@ -768,6 +768,44 @@ public:
 
 
 
+// 信号量。
+///////////////////////////////////// /////////////////////////////////////
+class csemp
+{
+private:
+    union semun  // 用于信号量操作的共同体。
+    {
+      int val;
+      struct semid_ds *buf;
+      unsigned short  *arry;
+    };
+
+    int   m_semid;         // 信号量id（描述符）。
+
+    // 如果把sem_flg设置为SEM_UNDO，操作系统将跟踪进程对信号量的修改情况，
+    // 在全部修改过信号量的进程（正常或异常）终止后，操作系统将把信号量恢复为初始值。
+    // 如果信号量用于互斥锁，设置为SEM_UNDO。
+    // 如果信号量用于生产消费者模型，设置为0。
+    short m_sem_flg;
+
+    csemp(const csemp &) = delete;             // 禁用拷贝构造函数。
+    csemp &operator=(const csemp &) = delete;  // 禁用赋值函数。
+public:
+    csemp():m_semid(-1){}
+
+    // 如果信号量已存在，获取信号量；如果信号量不存在，则创建它并初始化为value。
+    // 如果用于互斥锁，value填1，sem_flg填SEM_UNDO。
+    // 如果用于生产消费者模型，value填0，sem_flg填0。
+    bool init(key_t key,unsigned short value=1,short sem_flg=SEM_UNDO);
+    bool wait(short value=-1);    // 信号量的P操作，如果信号量的值是0，将阻塞等待，直到信号量的值大于0。
+    bool post(short value=1);     // 信号量的V操作。
+    int  getvalue();                       // 获取信号量的值，成功返回信号量的值，失败返回-1。
+    bool destroy();                       // 销毁信号量。
+    ~csemp();
+};
+///////////////////////////////////// /////////////////////////////////////
+
+
 
 // 进程心跳信息的结构体。
 ///////////////////////////////////// /////////////////////////////////////
